eecffcf633b8d604b2ef21d803743aa1591113d1
Sam-Dunne <cras.sdunne@gmail.com>
2021-01-25 14:30:55 -0600

    completed-cleaned comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//// Try 3: This worked. Refresh after Gameover alert ok clicked
+
 let cells = document.querySelectorAll('.row>div');
 let turn = 0;
 let gameOver = false;
@@ -55,17 +55,11 @@ function cellClicked(event) {
   //  ? "" : location.reload(); reloads the page when alerts 'ok' is clicked
   function declareWinnerFunc() {
     if (xWins && gameOver) {
-      // setTimeout(function() {
-      //   alert('X is the Winner') ? "" : location.reload();
-      // }, 100);
       h2.innerText = 'X done gave it to Ya !!!';
       setTimeout(function() {
       document.getElementById("Reset-Button").style.visibility='visible';
     }, 800);
     } else {
-      // setTimeout(function() {
-      //   alert('O is the Winner') ? "" : location.reload();
-      // }, 100);
       h2.innerText = 'O is the Winner!!!';
       setTimeout(function() {
         document.getElementById("Reset-Button").style.visibility='visible';
@@ -79,9 +73,6 @@ function cellClicked(event) {
     setTimeout(function() {
       document.getElementById("Reset-Button").style.visibility='visible';
     }, 800);
-    // setTimeout(function()  {
-    //   alert('It\'s a Draw') ? "" : location.reload();
-    // }, 100);
   }
 
   function winCheck() {
@@ -126,87 +117,4 @@ function cellClicked(event) {
 
 }
 
-// // if function winCheck evaluates both xWins and gameOver arguments as true,
-// // timeout delays alert until DOM painted. 
-// //  ? "" : location.reload(); reloads the page when alerts 'ok' is clicked
-// function declareWinnerFunc() {
-//   if (xWins && gameOver) {
-//       setTimeout(function() {
-//         alert('X is the Winner') ? "" : location.reload();
-//       }, 100);
-//   } else {
-//     setTimeout(function() {
-//       alert('O is the Winner') ? "" : location.reload();
-//     }, 100);
-//   }
-// }
-
-// //  Declares a draw from conditions set in function winCheck()
-// function tieFunc() {
-//   setTimeout(function()  {
-//     alert('It\'s a Draw') ? "" : location.reload();
-//   }, 100);
-// }
-
-// function winCheck() {
-
-//   //  X wins horizontal
-//   if ((cells[0].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X')
-//       || (cells[3].textContent === 'X' && cells[4].textContent === 'X' && cells[5].textContent === 'X')
-//       || (cells[6].textContent === 'X' && cells[7].textContent === 'X' && cells[8].textContent === 'X')
-//       ||
-//       //  X wins Vertical
-//       (cells[0].textContent === 'X' && cells[3].textContent === 'X' && cells[6].textContent === 'X')
-//       || (cells[1].textContent === 'X' && cells[4].textContent === 'X' && cells[7].textContent === 'X')
-//       || (cells[2].textContent === 'X' && cells[5].textContent === 'X' && cells[8].textContent === 'X')
-//       //  X wins Diagonal
-//       || (cells[0].textContent === 'X' && cells[4].textContent === 'X' && cells[8].textContent === 'X')
-//       || (cells[2].textContent === 'X' && cells[4].textContent === 'X' && cells[6].textContent === 'X')) {
-//       xWins = true;
-//       gameOver = true;
-//       declareWinnerFunc()
-//   } else if
-//       // O wins horizontal
-//       ((cells[0].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O')
-//       || (cells[3].textContent === 'O' && cells[4].textContent === 'O' && cells[5].textContent === 'O')
-//       || (cells[6].textContent === 'O' && cells[7].textContent === 'O' && cells[8].textContent === 'O')
-//       //  O wins Vertical
-//       || (cells[0].textContent === 'O' && cells[3].textContent === 'O' && cells[6].textContent === 'O')
-//       || (cells[1].textContent === 'O' && cells[4].textContent === 'O' && cells[7].textContent === 'O')
-//       || (cells[2].textContent === 'O' && cells[5].textContent === 'O' && cells[8].textContent === 'O')
-//       //  O wins Diagonal
-//       || (cells[0].textContent === 'O' && cells[4].textContent === 'O' && cells[8].textContent === 'O')
-//       || (cells[2].textContent === 'O' && cells[4].textContent === 'O' && cells[6].textContent === 'O')) {
-//       oWins = true;
-//       gameOver = true
-//       declareWinnerFunc()
-//   } else {
-//       if (turn === 9 && gameOver === false) {
-//           tieGame = true
-//           tieFunc()
-//       }
-//   }
-// }
-
-
-
-// function incTurn(turn) {
-//   turn++;
-//   console.log();
-// }
-// incTurn();
-// take turns
-// while (turn < 9) {
-//   turn++;
-//   console.log(turn);
-//   if (turn === 1 || 3 || 5 || 7 || 9) {
-//     event.target.textContent == 'x';
-//   } else if (turn === 2 || 4 || 6 || 8) {
-//     event.target.textContent == 'o';
-//   }
-// }
-
-
-// win condition
-
-// ?cell empty?                       
+     
